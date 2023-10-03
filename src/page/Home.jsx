8a0ad82b9a752dc0e39f453c438f4397fe5ad27e
Alexis Godet <alexis.godet05@gmail.com>
2023-10-03 10:39:29 +0200

    Bouton qui envoye sur la page game

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -4,6 +4,7 @@ import "../css/Home.css";
 
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
 export default class Home extends Component {
   render() {
@@ -17,7 +18,9 @@ export default class Home extends Component {
           La première version commerciale du jeu fut publiée en 1931 par la Starex Novelty Co. sous le nom de Salvo. Ce jeu est devenu populaire lors de son apparition en 1943 dans les publications américaines de divertissement de la Milton Bradley Company qui l'exploita sous la forme papier jusqu'en 1967, où elle sortit un jeu de plateau, puis en réalisa une version électronique en 1977.</p>
           <h2>Règles</h2>
           <p>La bataille navale oppose deux joueurs. Chaque joueur dispose de deux grilles carrées de côté 10, dont les lignes sont numérotées de 1 à 10 et les colonnes de A à J, ainsi que d'une flotte composée de quelques bateaux d'une à cinq cases de long. <br />L'une des grilles représente la zone contenant sa propre flotte. Au début du jeu, chaque joueur place ses bateaux sur sa grille. L'autre représente la zone adverse, où il cherchera à couler les bateaux de son adversaire.<br />Chaque joueur, à son tour, annonce une case (par exemple « B6 »), et son adversaire lui répond si le tir tombe à l'eau ou au contraire s'il touche un bateau. Dans ce dernier cas, il annonce « touché » s'il reste des cases intactes au bateau ciblé, et « touché-coulé » si non.</p>
-          <h1><button>Start game</button></h1>
+          <h1><Link to="/Game">
+            <button>Start game</button>
+            </Link></h1>
           <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         </center>
         <Footer />
