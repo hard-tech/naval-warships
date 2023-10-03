aea200792d0d4bbfdf6abae152c6f91c5a67864d
Alexis Godet <alexis.godet05@gmail.com>
2023-10-03 09:27:19 +0200

    Teste SINCScoreBoard

--- src/page/ScoreBoard.jsx
+++ src/page/ScoreBoard.jsx
@@ -6,13 +6,6 @@ export default class ScoreBoard extends Component {
   render() {
     return (
       <div>
-        <table>
-          <tr>
-            <td className='' >Bonjour, je suis votre première cellule</td>
-            
-            <td>Je suis 4ème</td>
-          </tr>
-        </table>
         <h1>let's go!</h1>
       </div>
     )
