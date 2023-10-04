4281f0b98e3e277f0040f4484405df3d81ff04b8
Alexis Godet <alexis.godet05@gmail.com>
2023-10-04 16:57:32 +0200

    fonction bateau presque terminer

--- src/components/Fonction_bateaux.jsx
+++ src/components/Fonction_bateaux.jsx
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 
-let x1 = 'B', y1 = '2', x2 = 'E', y4 = '2', type_of_ship = 'bateaux-size1_Size3';
-function retournement_bateau(x1,y1,x2,y2) {
-    const condition = x1 == x2;
-    return ( 
-      <div>
-        {condition ?(
-          alert('horizontale')
-        ): (
-          alert('verticale')
-        )
+const alphabetMajuscules = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let verticalite, sens
+function retournement_bateau(x1,y1,x2,y2,type_of_ship) {
+
+  for(let i = 0; i < alphabetMajuscules.length; i++){
+    if(x1==alphabetMajuscules[i]){
+      x1 = i+1
+    }
+  }
+  for(let i = 0; i < alphabetMajuscules.length; i++){
+    if(x2==alphabetMajuscules[i]){
+      x2 = i+1
+    }
+  }
+        if (x1==x2) {
+          verticalite = 'horizontale'
+          if (y1 < y2){
+            sens = 'droite'
           }
+          else {
+            sens = 'gauche'       
+          }
+        }
+        else {
+          verticalite = 'verticale'
+          if (x1 < x2){
+            sens = 'bas'
+          }
+          else {
+            sens = 'haut'         
+          }
+        }
+    return ( 
+      <div className="">
+
       </div>
     )
 }
+
+retournement_bateau('B','5','B','2','bateaux-size1_Size3');
+
 export default retournement_bateau;
