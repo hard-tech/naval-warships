28348a6c355d3178f1245e4d4e727444a9b45698
Ahmad Jaber <ahamd.jaber010@yahoo.com>
2023-10-05 00:36:38 +0200

    Fini system Flag + commencement du placement des bateaux

---
+++ src/function/PrintShips.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const alphabetMajuscules = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let verticalite, sens
+function PrintShips(xA, yA, xB, yb, ShipsX) {
+  // Affiche les bateaux dans les bonne case avec les bonnes images (Gestion de className)
+}
+
+export default PrintShips;
--- src/page/Game.jsx
+++ src/page/Game.jsx
@@ -1,5 +1,4 @@
 import React, { Component, useState } from "react";
-import Fonction_bateau from "../components/Fonction_bateaux";
 import "../css/Game.css";
 import {
   ref,
@@ -28,57 +27,38 @@ import Bateaux2S4 from "../Images/EnsembleDesBateaux/bateaux-2_Size-4.png";
 
 import FlagIcon from "../Images/icon/Icon_Flag.png";
 
+import PrintShips from "../function/PrintShips";
+
 const playCode = localStorage.playCode;
 const playerName = localStorage.playerName;
 const playerNum = localStorage.playerNum;
 
-var PinedX, PinedY;
+var PinedX,
+  PinedY,
+  SetShipsOn,
+  ShipNumber = 1;
 
 function Game() {
-
-
-  // const [tableData, setTableData] = useState(
-  //   Array.from({ length: 10 }, () => Array(10).fill(""))
-  // );
-
-  // // Utilisez l'état pour tablePin
-  // const [tablePin, setTablePin] = useState([]);
-
-  // function PinThis(x, y) {
-  //   // Mettez à jour la copie du tableau en fonction des coordonnées x et y
-  //   const coordinate = `${x}-${y}`;
-
-  //   if (tablePin.includes(coordinate)) {
-  //     alert(`Les coordonnées du drapeau pour x:${x} et y:${y} existent déjà.`);
-  //   } else {
-  //     // Utilisez setTablePin pour mettre à jour le tableau d'états
-  //     setTablePin((prevTablePin) => [...prevTablePin, coordinate]);
-  //   }
-
-  //   // ... (le reste de votre code)
-  // }
-
-  // // ... (le reste de votre code)
-
-
   const [tableData, setTableData] = useState(
     Array.from({ length: 10 }, () => Array(10).fill(""))
   );
-  var tablePin = [];
+  const [tablePin, setTablePin] = useState([]);
 
   function PinThis(x, y) {
     // Mettez à jour la copie du tableau en fonction des coordonnées x et y
     PinedX = x;
     PinedY = y;
 
-    const isCoordinateExist = (x, y) => {
-      return tablePin.includes(`${x}-${y}`);
-    };
+    const coordinate = `${x}-${y}`;
 
-    if (isCoordinateExist(x, y)) {
-      alert(`Les coordonnées la flag d'x:${x} et d'y:${y} existent déjà.`);
+    if (tablePin.includes(coordinate)) {
+      // si il y a déjà un drapaux l'enlever du tablaux
+      setTablePin((prevTablePin) =>
+        prevTablePin.filter((coord) => coord !== coordinate)
+      );
     } else {
-      tablePin.push(`${x}-${y}`);
+      // Utilisez setTablePin pour mettre à jour le tableau d'états
+      setTablePin((prevTablePin) => [...prevTablePin, coordinate]);
     }
 
     // alert(`Pin on X${PinedX+1} & Y${PinedY+1}`)
@@ -87,39 +67,105 @@ function Game() {
     tableData.forEach((pinFlagIsIn) => {});
   }
 
+  function convertLettreToChiffre(lettre) {
+    // Vérifiez si la lettre est valide (A à J)
+    if (/^[A-J]$/.test(lettre)) {
+      // Convertissez la lettre en chiffre (1 à 10)
+      const chiffre = lettre.charCodeAt(0) - "A".charCodeAt(0) + 1;
+      return chiffre;
+    } else {
+      // Gérez le cas où la lettre n'est pas valide
+      console.error("Lettre non valide");
+      return null;
+    }
+  }
+
+  // Fonction pour extraire la lettre de la chaîne //
+  function extractLettre(coord) {
+    // Utilisez une expression régulière pour extraire la première lettre de la chaîne
+    const lettreMatch = coord.match(/[A-Za-z]/);
+
+    if (lettreMatch) {
+      return lettreMatch[0].toUpperCase(); // Convertissez la lettre en majuscule
+    } else {
+      console.error("Lettre non trouvée dans la coordonnée.");
+      return null;
+    }
+  }
+  // Fonction pour extraire les chiffres de la chaîne//
+  function extractChiffre(coord) {
+    // Utilisez une expression régulière pour extraire les chiffres de la chaîne
+    const chiffreMatch = coord.match(/\d+/);
+
+    if (chiffreMatch) {
+      return Number(chiffreMatch[0]);
+    } else {
+      console.error("Chiffres non trouvés dans la coordonnée.");
+      return null;
+    }
+  }
+
+  // Fonction pour extraire la lettre de la chaîne
+  function extractLettre(coord) {
+    const lettreMatch = coord.match(/[A-Za-z]/);
+
+    if (lettreMatch) {
+      return lettreMatch[0].toUpperCase();
+    } else {
+      console.error("Lettre non trouvée dans la coordonnée.");
+      return null;
+    }
+  }
+
   function SetShips(ShipsX) {
-    alert("Voudevez selectioner l'endroit où le bateaux sera placer.");
+    if (true) {
+      var shipStart = prompt(
+        `Tapper le point de commencement de votre bateaux N°${ShipNumber}`
+      );
+      var shipEnd = prompt(
+        `Tapper le point où Fini votre bateaux N°${ShipNumber} (Vertical et Horizontal seulement)`
+      );
 
-    var xA = 4;
-    var yA = 2;
-    var xB = 8;
-    var yB = 2;
+      ShipNumber++;
 
-    set(
-      ref(
-        db,
-        `Games/CodeToPlay_${playCode}/CoordinatedShips/ShipsPlayer${playerNum}/${ShipsX}`
-      ),
-      {
-        xA: xA,
-        yA: yA,
-        xB: xB,
-        yB: yB,
-      }
-    )
-      .then(() => {
-        console.log("Succses Send ;)");
-        alert("Succses Send ;)");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      var xA = convertLettreToChiffre(extractLettre(shipStart));
+      var yA = extractChiffre(shipStart);
+      var xB = convertLettreToChiffre(extractLettre(shipEnd));
+      var yB = extractChiffre(shipEnd);
+      alert(`${xA}, ${xB}, ${yA}, ${yB}`);
+
+      // Récuper XA et XB du ship en cour.
+
+      // PrintShips(xA, yA, xB, yB, ShipsX);
+
+      set(
+        ref(
+          db,
+          `Games/CodeToPlay_${playCode}/CoordinatedShips/ShipsPlayer${playerNum}/${ShipsX}`
+        ),
+        {
+          xA: xA,
+          yA: yA,
+          xB: xB,
+          yB: yB,
+        }
+      )
+        .then(() => {
+          console.log("Succses Send ;)");
+          alert("Succses Send ;)");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      alert("Ce bateaux est déjà placer !");
+    }
   }
 
   function Flag() {
     return (
       <div>
-        <img src={FlagIcon} alt="" />
+        <img src={FlagIcon} alt="" width={20} />
       </div>
     );
   }
@@ -133,7 +179,6 @@ function Game() {
             <div className="d-flex flex-warp blocBoat">
               <div id="titleBoat">
                 <h3 className="text-dark ">Placez vos bateaux</h3>
-                <Fonction_bateau></Fonction_bateau>
                 <section id="boxBoat">
                   <img
                     src={Bateaux1S2}
@@ -166,136 +211,22 @@ function Game() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>A</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>B</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>C</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>D</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>E</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>F</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>G</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>H</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>I</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>J</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
+                    {tableData.map((row, rowIndex) => (
+                      <tr key={rowIndex}>
+                        <td>{String.fromCharCode(65 + rowIndex)}</td>
+                        {row.map((cell, colIndex) => (
+                          <td key={colIndex}>
+                            {/* {cell === "" && (
+                              <div className={`${colIndex}-${rowIndex}`}>
+                                {tablePin.includes(`${colIndex}-${rowIndex}`) && (
+                                  <Flag />
+                                )}
+                              </div>
+                            )} */}
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
                 <button className="btnReady">Prêt</button>
@@ -330,14 +261,14 @@ function Game() {
                         onClick={() => {
                           // Vérifiez si la cellule est vide avant d'appeler la fonction PinThis
                           if (cell === "") {
-                            PinThis(rowIndex, colIndex);
+                            PinThis(colIndex, rowIndex);
                           }
                         }}
                       >
                         {/* Utilisez une condition pour afficher <Flag /> si la classe est '1-1' */}
                         {cell === "" && (
-                          <div className={`${rowIndex}-${colIndex}`}>
-                            {tablePin.includes(`${rowIndex}-${colIndex}`) && (
+                          <div className={`${colIndex}-${rowIndex}`}>
+                            {tablePin.includes(`${colIndex}-${rowIndex}`) && (
                               <Flag />
                             )}
                           </div>
