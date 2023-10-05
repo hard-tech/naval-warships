fa3125cd4f28a332378af0c2acfec7e2139bd2d5
Mathias.G <mathiasghanem05@gmail.com>
2023-10-05 11:21:22 +0200

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,8 +6,6 @@ export default class Nav extends Component {
     return (
       <nav className='d-flex justify-content-around align-content-center align-items-center mb-5 mt-2'>
         <img src={IconSite} alt="Ico" width={70} style={{border:'5px solid', borderRadius:'50%'}} />
-        <center><h1>Naval warships</h1></center>
-        <div className=""></div>
       </nav>
     )
   }
