69ce1fa9db6725e0188975ac252154990c260189
Alexis Godet <alexis.godet05@gmail.com>
2023-10-05 11:17:34 +0200

    home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -14,7 +14,7 @@ export default class Home extends Component {
       <div>
         <Nav />
           <h1>Règles</h1>
-          <p id="textRules">La bataille navale oppose deux joueurs. Chaque joueur dispose de deux grilles carrées de côté 10, dont les lignes sont numérotées de 1 à 10 et les colonnes de A à J, ainsi que d'une flotte composée de quelques bateaux d'une à cinq cases de long. <br />L'une des grilles représente la zone contenant sa propre flotte. Au début du jeu, chaque joueur place ses bateaux sur sa grille. L'autre représente la zone adverse, où il cherchera à couler les bateaux de son adversaire.<br />Chaque joueur, à son tour, annonce une case (par exemple « B6 »), et son adversaire lui répond si le tir tombe à l'eau ou au contraire s'il touche un bateau. Dans ce dernier cas, il annonce « touché » s'il reste des cases intactes au bateau ciblé, et « touché-coulé » si non.</p>
+          <p id="textRules">Etre le premier a remplir les 100 cases avec des drapaux rien de plus simple</p>
           <FormConnect />
         <Footer />
       </div>
