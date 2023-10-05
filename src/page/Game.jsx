4d4803324045d7682db850c032509742eb5c42fd
Alexis Godet <alexis.godet05@gmail.com>
2023-10-05 10:35:58 +0200

    dev clean

--- src/page/Game.jsx
+++ src/page/Game.jsx
@@ -54,7 +54,7 @@ function Game() {
     PinedX = x;
     PinedY = y;
 
-    const coordinate = `${x}-${y}`;
+    const coordinate = `${x}-${y}`; 
 
     if (tablePin.includes(coordinate)) {
       // si il y a déjà un drapaux l'enlever du tablaux
@@ -145,24 +145,17 @@ function Game() {
 
       PrintShips(xA, yA, xB, yB, ShipsX, Seiz);
 
-      set(
-        ref(
-          db,
-          `Games/CodeToPlay_${playCode}/CoordinatedShips/ShipsPlayer${playerNum}/${ShipsX}`
-        ),
-        {
-          xA: xA,
-          yA: yA,
-          xB: xB,
-          yB: yB,
-        }
-      )
-        .then(() => {
-          console.log("Succses Send ;)");
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      const updates = {};
+      updates[
+        `Games/CodeToPlay_${playCode}/GameInfo/CoordinatedShips/ShipsPlayer${playerNum}/${ShipsX}`
+      ] = 
+      {
+        xA: xA,
+        yA: yA,
+        xB: xB,
+        yB: yB,
+      };
+      update(ref(db), updates);
 
         // remise à 0 des Cordonée du bateaux //
         xA = 0;xB = 0;yA = 0;yB = 0
@@ -170,370 +163,9 @@ function Game() {
 
   function Flag() {
     return (
-      <table id="finished" className="my-3">
-        <tbody>
-          <tr>
-            <td
-                id="1A"
-              onClick={() => Game('A',1)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="1A"></div>
-            </td>
-            <td
-              id="1B"
-              onClick={() => Game('B',1)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="1B"></div>
-            </td>
-            <td
-              id="1C"
-              onClick={() => Game('C',1)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="1C"></div>
-            </td>
-            <td
-              id="1D"
-              onClick={() => Game('D',1)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="1D"></div>
-            </td>
-            <td
-              id="1E"
-              onClick={() => Game('E',1)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="1E"></div>
-            </td>
-            <td
-              id="1F"
-              onClick={() => Game('F',1)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="1F"></div>
-            </td>
-            <td
-              id="1G"
-              onClick={() => Game('G',1)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="1G"></div>
-            </td>
-            <td
-              id="1H"
-              onClick={() => Game('H',1)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="1H"></div>
-            </td>
-            <td
-              id="1I"
-              onClick={() => Game('I',1)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="1I"></div>
-            </td>
-            <td
-              id="1J"
-              onClick={() => Game('J',1)}
-              className="p-4 border-4 border-light"
-            >
-            <div id="1J"></div>
-            </td>
-          </tr>
-          <tr>
-            <td
-              id="2A"
-              onClick={() => Game('A',2)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="Case1_1" className={Class_Case1_1}></div>
-            </td>
-            <td
-              id="2B"
-              onClick={() => Game('B',2)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="2B"></div>
-            </td>
-            <td
-              id="2C"
-              onClick={() => Game('C',2)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="2C"></div>
-            </td>
-            <td
-              id="2D"
-              onClick={() => Game('D',2)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="2D"></div>
-            </td>
-            <td
-              id="2E"
-              onClick={() => Game('E',2)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="2E"></div>
-            </td>
-            <td
-              id="2F"
-              onClick={() => Game('F',2)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="2F"></div>
-            </td>
-            <td
-              id="2G"
-              onClick={() => Game('G',2)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="2G"></div>
-            </td>
-            <td
-              id="2H"
-              onClick={() => Game('H',2)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="2H"></div>
-            </td>
-            <td
-              id="2I"
-              onClick={() => Game('I',2)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="2I"></div>
-            </td>
-            <td
-              id="2J"
-              onClick={() => Game('J',2)}
-              className="p-4 border-4 border-light"
-            >
-                <div id="2J"></div>
-            </td>
-          </tr>
-          <tr>
-            <td
-              id="3A"
-              onClick={() => Game('A',3)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="3A"></div>
-            </td>
-            <td
-              id="3B"
-              onClick={() => Game('B',3)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="3B"></div>
-            </td>
-            <td
-              id="3C"
-              onClick={() => Game('C',3)}
-              className="p-4 border-4 border-light"
-            >
-             <div id="3C"></div>
-            </td>
-            <td
-              id="3D"
-              onClick={() => Game('D',3)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="3D"></div>
-            </td>
-            <td
-              id="3E"
-              onClick={() => Game('E',3)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="3E"></div>
-            </td>
-            <td
-              id="3F"
-              onClick={() => Game('F',3)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="3F"></div>
-            </td>
-            <td
-              id="3G"
-              onClick={() => Game('G',3)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="3G"></div>
-            </td>
-            <td
-              id="3H"
-              onClick={() => Game('H',3)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="3H"></div>
-            </td>
-            <td
-              id="3I"
-              onClick={() => Game('I',3)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="3I"></div>
-            </td>
-            <td
-              id="3J"
-              onClick={() => Game('J',3)}
-              className="p-4 border-4 border-light"
-            >
-                <div id="3J"></div>
-            </td>
-          </tr>
-          <tr>
-            <td
-              id="4A"
-              onClick={() => Game('A',4)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="4A"></div>
-            </td>
-            <td
-              id= "4B"
-              onClick={() => Game('B',4)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="4B"></div>
-            </td>
-            <td
-              id="4C"
-              onClick={() => Game('C',4)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="4C"></div>
-            </td>
-            <td
-              id="4D"
-              onClick={() => Game('D',4)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="4D"></div>
-            </td>
-            <td
-              id="4E"
-              onClick={() => Game('E',4)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="4E"></div>
-            </td>
-            <td
-              id="4F"
-              onClick={() => Game('F',4)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="4F"></div>
-            </td>
-            <td
-              id="4G"
-              onClick={() => Game('G',4)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="4G"></div>
-            </td>
-            <td
-              id="4H"
-              onClick={() => Game('H',4)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="4H"></div>
-            </td>
-            <td
-              id="4I"
-              onClick={() => Game('I',4)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="4I"></div>
-            </td>
-            <td
-              id="4J"
-              onClick={() => Game('J',4)}
-              className="p-4 border-4 border-light"
-            >
-                <div id="4J"></div>
-            </td>
-          </tr>
-          <tr>
-            <td
-              id="5A"
-              onClick={() => Game('A',5)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="5A"></div>
-            </td>
-            <td
-              id="5B"
-              onClick={() => Game('B',5)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="5B"></div>
-            </td>
-            <td
-              id="5C"
-              onClick={() => Game('C',5)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="5C"></div>
-            </td>
-            <td
-              id="5D"
-              onClick={() => Game('D',5)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="5D"></div>
-            </td>
-            <td
-              id="5E"
-              onClick={() => Game('E',5)}
-              className="p-4 border-4 border-light border-top border-bottom"
-            >
-              <div id="5E"></div>
-            </td>
-            <td
-              id="5F"
-              onClick={() => Game('F',5)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="5F"></div>
-            </td>
-            <td
-              id="5G"
-              onClick={() => Game('G',5)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="5G"></div>
-            </td>
-            <td
-              id="5H"
-              onClick={() => Game('H',5)}
-              className="p-4 border-4 border-light"
-            >
-              <div id="5H"></div>
-            </td>
-            <td
-              id="5I"
-              onClick={() => Game('I',5)}
-              className="p-4 border-4 border-light border-end border-start"
-            >
-              <div id="5I"></div>
-            </td>
-            <td
-              id="5J"
-              onClick={() => Game('J',5)}
-              className="p-4 border-4 border-light"
-            >
-                <div id="5J"></div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <img src={FlagIcon} alt="" width={20} />
+      </div>
     );
   }
 
@@ -563,7 +195,7 @@ function Game() {
                   />
                   <img
                     src={Bateaux1S3}
-                    onClick={() => SetShips("Ship1S2", 3)}
+                    onClick={() => SetShips("Ship1S3", 3)}
                     alt="#"
                   />
                                     <img
@@ -685,3 +317,4 @@ function Game() {
     </div>
   );
 }
+export default Game;
